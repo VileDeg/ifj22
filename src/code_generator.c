b4041d79efd4d95b7dfe971395676e29300267d4
xshevc01 <swimmer030203@gmail.com>
2022-10-09 17:27:05 +0200

    Added strlen and chr

--- src/code_generator.c
+++ src/code_generator.c
@@ -1,3 +1,30 @@
-//
-// Created by aleksandr on 9.10.22.
-//
+/**
+* GENERATOR OF BUILT-IN FUNCTIONS
+*/
+
+/// function strlen(string $𝑠) : int
+#define FUNCTION_STRLEN                 \
+        "# Function strlen\n"           \
+        "LABEL @strlen\n"               \
+        "PUSHFRAME\n"                   \
+        "DEFVAR LF@return\n"            \
+        "STRLEN LF@return LF@-0\n"      \
+        "POPFRAME\n"                    \
+        "RETURN\n"
+
+/// function chr(int $i) : string
+#define FUNCTION_CHR \
+        "# Function chr\n"                      \
+        "LABEL @chr\n"                          \
+        "PUSHFRAME\n"                           \
+        "DEFVAR LF@return\n"                    \
+        "MOVE LF@return string@\n"              \
+        "DEFVAR LF@cond\n"                      \
+        "GT LF@cond LF@-0 int@255\n"            \
+        "JUMPIFEQ $chr_end LF@cond bool@true\n" \
+        "LT LF@cond LF@-0 int@0\n"              \
+        "JUMPIFEQ $chr_end LF@cond bool@true\n" \
+        "INT2CHAR LF@return LF@-0\n"            \
+        "LABEL @chr_end"                        \
+        "POPFRAME\n"                            \
+        "RETURN\n"
