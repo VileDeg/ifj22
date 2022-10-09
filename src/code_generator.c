bcfba035a8662dda7332bff44d1246ac6610e6cc
xshevc01 <swimmer030203@gmail.com>
2022-10-09 18:17:14 +0200

    Added ord

--- src/code_generator.c
+++ src/code_generator.c
@@ -12,6 +12,27 @@
         "POPFRAME\n"                    \
         "RETURN\n"
 
+/// function ord(string $c) : int
+#define FUNCTION_ORD                                \
+        "# Function ord\n"                          \
+        "LABEL @ord\n"                              \
+        "PUSHFRAME\n"                               \
+        "DEFVAR LF@return\n"                        \
+        "MOVE LF@return int@0\n"                    \
+        "DEFVAR LF@length\n"                        \
+        "CREATEFRAME\n"                             \
+        "DEFVAR TF@-0\n"                            \
+        "MOVE TF@-0 LF@-0\n"                        \
+        "CALL @strlen\n"                            \
+        "MOVE LF@length TP@return\n"                \
+        "DEFVAR LF@cond\n"                          \
+        "GT LF@cond LF@length int@0\n"              \
+        "JUMPIFNEQ @ord_end LF@length bool@true\n"  \
+        "STRI2INT LF@return LF@-0 int@0\n"          \
+        "LABEL @ord_end\n"                          \
+        "POPFRAME\n"                                \
+        "RETURN\n"
+
 /// function chr(int $i) : string
 #define FUNCTION_CHR \
         "# Function chr\n"                      \
